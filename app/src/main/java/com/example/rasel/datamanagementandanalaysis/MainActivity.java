db805e2799e8b559ba64d775e8a1b2d87aa008d0
Al-Rasel <mdalrasel1@gmail.com>
2016-09-06 23:19:42 +0600

    firebase is added

--- app/src/main/java/com/example/rasel/datamanagementandanalaysis/MainActivity.java
+++ app/src/main/java/com/example/rasel/datamanagementandanalaysis/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-String TAG= "MainActivity";
+    String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
