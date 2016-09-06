a746bd47123fb7e8f52a82f559c88d8e58ec75d3
Al-Rasel <mdalrasel1@gmail.com>
2016-09-06 22:33:11 +0600

    first check of successfully push

--- app/src/main/java/com/example/rasel/datamanagementandanalaysis/MainActivity.java
+++ app/src/main/java/com/example/rasel/datamanagementandanalaysis/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.rasel.datamanagementandanalaysis;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+String TAG= "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: githubCheck");
     }
 }
